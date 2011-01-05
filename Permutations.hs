b293524fdf8d1c8692d999ed484de6ceab6b3c68
Helge S. Holm <helge.holm@gmail.com>
2011-01-05 16:08:41 +0100

    Added test data for experimenting with the permutation code.

--- Permutations.hs
+++ Permutations.hs
@@ -14,3 +14,25 @@ permuteTest ss = do
   profiles <- find ss "profiles"
   let (Section (Name n) _) = profiles
   return [n]
+
+----------------------------------
+-- TEST DATA
+
+posate = SimpleOrRestrictor . SimpleAndRestrictor 
+       . PosateRestrictor . NameRestrictor . Name
+nameCond = SimpleCond . NameAtom . Name
+aName = NameAtom . Name
+
+-- test data
+sampleProfiles :: Section
+sampleProfiles = Section (Name "profiles") [
+  (ProperAssign (nameCond "color") [
+      nameCond "red",
+      nameCond "white"]),
+  (ProperAssign (nameCond "taste") [
+    nameCond "apple",
+    RestrCond (aName "strawberry") (posate "red")]),
+  (ProperAssign (RestrCond (aName "bleached") (posate "white")) [
+      nameCond "yes",
+      nameCond "no"])
+  ]
