86e8527b5d48f58daba2f10efc41e7f03b4b6ecd
Helge S. Holm <helge.holm@gmail.com>
2011-01-05 22:36:58 +0100

    Wrote valid :: [Name] -> Cond -> Bool and added tests for it.

--- Permutations.hs
+++ Permutations.hs
@@ -17,17 +17,44 @@ permuteTest ss = do
 
 -- Given a set of defined variants and a condition, is the condition valid?
 valid :: [Name] -> Cond -> Bool
-valid = undefined
+valid _ (SimpleCond _) =
+  True
+valid defs (RestrCond _ or) = evalOr or
+  where evalOr (SimpleOrRestrictor a) = evalAnd a
+        evalOr (ProperOrRestrictor a o) = (evalAnd a) || (evalOr o)
+        evalAnd (SimpleAndRestrictor n) = evalNot n
+        evalAnd (ProperAndRestrictor n a) = (evalNot n) && (evalAnd a)
+        evalNot (NegateRestrictor inner) = (not . evalInner) inner
+        evalNot (PosateRestrictor inner) = evalInner inner
+        evalInner (NameRestrictor n) = n `elem` defs
+        evalInner (ParenRestrictor o) = evalOr o
 
 ----------------------------------
 -- TEST DATA
 
-posate = SimpleOrRestrictor . SimpleAndRestrictor 
-       . PosateRestrictor . NameRestrictor . Name
+a_posate = SimpleAndRestrictor . PosateRestrictor . NameRestrictor . Name
+o_posate = SimpleOrRestrictor . a_posate
+a_negate = SimpleAndRestrictor . NegateRestrictor . NameRestrictor . Name
+o_negate = SimpleOrRestrictor . a_negate
 nameCond = SimpleCond . NameAtom . Name
 aName = NameAtom . Name
 
--- test data
+test_valid :: Bool
+test_valid = and [
+  valid [Name "derp", Name "white"] bleachedIfWhite,
+  not (valid [Name "derp"] bleachedIfWhite),
+  valid [Name "derp"] derpOrHerpButNotBurp,
+  valid [Name "herp"] derpOrHerpButNotBurp,
+  valid [Name "derp", Name "herp"] derpOrHerpButNotBurp,
+  not (valid [Name "derp", Name "burp"] derpOrHerpButNotBurp)]
+  where bleachedIfWhite = RestrCond (aName "bleached") (o_posate "white")
+        derpOrHerpButNotBurp = RestrCond (aName "_") (
+          SimpleOrRestrictor (ProperAndRestrictor derpOrHerp notBurp))
+        notBurp = a_negate "burp"
+        derpOrHerp = (PosateRestrictor . ParenRestrictor)
+                     (ProperOrRestrictor (a_posate "herp") (o_posate "derp"))
+
+-- for manual testing
 sampleProfiles :: [Assign]
 sampleProfiles = [
   (ProperAssign (nameCond "color") [
@@ -35,8 +62,8 @@ sampleProfiles = [
       nameCond "white"]),
   (ProperAssign (nameCond "taste") [
     nameCond "apple",
-    RestrCond (aName "strawberry") (posate "red")]),
-  (ProperAssign (RestrCond (aName "bleached") (posate "white")) [
+    RestrCond (aName "strawberry") (o_posate "red")]),
+  (ProperAssign (RestrCond (aName "bleached") (o_posate "white")) [
       nameCond "yes",
       nameCond "no"])
   ]
