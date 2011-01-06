46af12656960716be7f0cb9344f9f2b7de260c92
Helge S. Holm <helge.holm@gmail.com>
2011-01-06 04:12:57 +0100

    Created function for enumerating all valid profile fixes.

--- Permutations.hs
+++ Permutations.hs
@@ -2,18 +2,38 @@ module Permutations where
 
 import Data
 
-find :: (Monad a) => [Section] -> String -> a Section
-find [] id =
-  fail ("no section named " ++ id)
-find (s@(Section (Name name) _):ss) id =
-  if name == id then return s
-                else find ss id
+permute :: [Assign] -> [[Name]]
+permute as =
+  filter (profilesFit as) selections
+  where selections :: [[Name]] 
+        selections = zeroOrOneFromEach [] groupedNames
+        groupedNames :: [[Name]]
+        groupedNames = map (concat . assNames) as
+        assNames (NotReallyAssign _) = []
+        assNames (ProperAssign _ ns) = map extractName ns
+        extractName (SimpleCond (NameAtom n))  = [n]
+        extractName (RestrCond (NameAtom n) _) = [n]
+        extractName _                          = []
+
+-- Returns all possible combinations.
+zeroOrOneFromEach :: [a] -> [[a]] -> [[a]]
+zeroOrOneFromEach acc [] = [acc]
+zeroOrOneFromEach acc (g:gs) = x ++ xs
+  where x = zeroOrOneFromEach acc gs
+        xs = concat $ map (\x -> zeroOrOneFromEach (x:acc) gs) g
 
 permuteTest :: (Monad a) => [Section] -> a [String]
 permuteTest ss = do
   profiles <- find ss "profiles"
   let (Section (Name n) _) = profiles
   return [n]
+
+find :: (Monad a) => [Section] -> String -> a Section
+find [] id =
+  fail ("no section named " ++ id)
+find (s@(Section (Name name) _):ss) id =
+  if name == id then return s
+                else find ss id
 
 -- Given a set of defined variants and a condition, is the condition valid?
 valid :: [Name] -> Cond -> Bool
@@ -28,6 +48,27 @@ valid defs (RestrCond _ or) = evalOr or
         evalNot (PosateRestrictor inner) = evalInner inner
         evalInner (NameRestrictor n) = n `elem` defs
         evalInner (ParenRestrictor o) = evalOr o
+
+-- Do we have 1 and only 1 valid definition from this profile?
+profileFit :: [Name] -> Assign -> Bool
+profileFit _ (NotReallyAssign _) = False
+profileFit defs (ProperAssign _ vars) =
+  let validVarNames = (concat . map extractName) vars in
+  length (filter (`elem` defs) validVarNames) == 1
+  where extractName (SimpleCond (NameAtom n))    = [n]
+        extractName (SimpleCond _)               = []
+        extractName (RestrCond (StringAtom _) _) = []
+        extractName r@(RestrCond (NameAtom n) _) =
+          if valid defs r then [n]
+                          else []
+
+-- Is this list of defined variants a valid permutation of the profile values?
+profilesFit :: [Assign] -> [Name] -> Bool
+profilesFit as defs = (length defs == length validAssigns
+                       && and (map (profileFit defs) validAssigns))
+  where validAssigns = filter validAssP as
+        validAssP (NotReallyAssign _) = False
+        validAssP (ProperAssign c _)  = valid defs c
 
 ----------------------------------
 -- TEST DATA
@@ -46,7 +87,8 @@ test_valid = and [
   valid [Name "derp"] derpOrHerpButNotBurp,
   valid [Name "herp"] derpOrHerpButNotBurp,
   valid [Name "derp", Name "herp"] derpOrHerpButNotBurp,
-  not (valid [Name "derp", Name "burp"] derpOrHerpButNotBurp)]
+  not (valid [Name "derp", Name "burp"] derpOrHerpButNotBurp)
+  ]
   where bleachedIfWhite = RestrCond (aName "bleached") (o_posate "white")
         derpOrHerpButNotBurp = RestrCond (aName "_") (
           SimpleOrRestrictor (ProperAndRestrictor derpOrHerp notBurp))
@@ -64,6 +106,6 @@ sampleProfiles = [
     nameCond "apple",
     RestrCond (aName "strawberry") (o_posate "red")]),
   (ProperAssign (RestrCond (aName "bleached") (o_posate "white")) [
-      nameCond "yes",
-      nameCond "no"])
+      nameCond "yesbleached",
+      nameCond "nobleached"])
   ]
